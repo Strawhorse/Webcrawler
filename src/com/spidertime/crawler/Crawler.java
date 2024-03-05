2aedc235c164ec4e0492ebe0a712e7b94d3751d9
Sean B <strawhorse@gmail.com>
2024-03-05 20:34:11 +0000

    Creating method for what site to visit next

--- src/com/spidertime/crawler/Crawler.java
+++ src/com/spidertime/crawler/Crawler.java
@@ -2,16 +2,28 @@ package com.spidertime.crawler;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class Crawler {
 
     private static final int MAX_SEARCH_PAGES = 10;
-    private final Set<String> pagesVisited = new HashSet<String>();
-    private LinkedList<String> pagesToVisit = new LinkedList<String>();
+    private Set<String> pagesVisited = new HashSet<String>();
+    private List<String> pagesToVisit = new LinkedList<String>();
 
 //    use a Set to store pages visited because it only stores one copy of each item
+//  Lists have special methods such as adding an entry to the end of a list or adding an entry to the beginning
 
+
+//    method to choose the next site to visit based on what's stored in the set and list above
+
+    private String nextURLToVisit() {
+        String nextURLToVisit;
+        do {
+            nextURLToVisit = this.pagesToVisit.remove(0);
+
+        }
+    }
     
 
 
